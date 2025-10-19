7fc4db7f166aeab82d7ef7405c3aed2486816744
Charbel Assaker <charbel.assaker@coderaas.com>
2025-10-19 17:41:01 +0300

    f

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
-import pg, { Client } from "pg";
 import dotenv from "dotenv";
+import { Client } from "pg";
 dotenv.config();
-const { Pool } = pg;
 
-const pool = new Client({
+const client = new Client({
   connectionString: process.env.DB_URL,
 });
-export default pool;
+client.connect(); // REQUIRED
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -88,5 +88,6 @@ app.post("/crumbs", auth, (req, res, next) => {
 });
 
 app.listen(3000, () => {
+  db.connect();
   console.log("All 3000 ears");
 });
